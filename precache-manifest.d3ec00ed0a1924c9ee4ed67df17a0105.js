b7697eadf379cc087b639eadbe8e9200b6e40966
NGAN Yuen Ling <62645099+belloah@users.noreply.github.com>
2020-07-01 10:49:41 +0800

    Updates

---
+++ precache-manifest.d3ec00ed0a1924c9ee4ed67df17a0105.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6c0aa85226c36f16d8514477e720fef",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "2e4e51971618e1510525",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d7cfbdd64c30a482204b",
+    "url": "/react/static/js/2.1e20d17f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.1e20d17f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e4e51971618e1510525",
+    "url": "/react/static/js/main.b7bdb54c.chunk.js"
+  },
+  {
+    "revision": "990ae4f7d8b1356e8c98",
+    "url": "/react/static/js/runtime-main.cab0e815.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
